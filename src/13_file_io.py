7c7cfd6a4c20f6a685a9d7397256428d46ce6faa
MicahJank <MicahJank@users.noreply.github.com>
2020-05-06 12:27:21 -0500

    almost finished with file_io

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,10 +10,17 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+with open("./src/foo.txt", "r") as foo:
+    read_data = foo.read()
+    print(read_data)
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
+with open('./src/bar.txt', 'w') as bar:
+    bar.write("I have a pen, I have an apple - uh - apple pen!")
+    bar.write("I have a pen, I have pineapple - uh - pineapple pen!")
+    bar.write("Apple pen...Pineapple pen.... - uh - Pen pineapple apple pen!")
 
 # YOUR CODE HERE
